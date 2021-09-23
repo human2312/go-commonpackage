51e88c93992e29c16ea0995db66435d5633a851d
lemy <human2312@qq.com>
2021-09-23 19:50:02 +0800

    Hello World

---
+++ common.go
@@ -0,0 +1,17 @@
+/**
+ * @Author: 作者名
+ * @Description: 描述
+ * @File: common.go //文件名称
+ * @Version: 1.0.0
+ * @Date: 2021/9/23 7:48 下午  //时间
+ * @Software : GoLand //开发软件【可选】
+ */
+package common
+
+type Common struct {
+
+}
+
+func (this *Common) Hello() string{
+	return "Hello World"
+}
