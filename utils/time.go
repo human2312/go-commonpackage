64aa4f4153f8cfdaa223e24a2075e8a6e9b002bd
lemy <human2312@qq.com>
2021-09-23 20:44:18 +0800

    Time

---
+++ utils/time.go
@@ -0,0 +1,18 @@
+/**
+ * @Author: 作者名
+ * @Description: 描述
+ * @File: tims //文件名称
+ * @Version: 1.0.0
+ * @Date: 2021/9/23 8:41 下午  //时间
+ * @Software : GoLand //开发软件【可选】
+ */
+package utils
+
+import "time"
+
+type Time struct {
+}
+
+func (this *Time) now() string {
+	return time.Now().Format("2006-01-02 15:04:05")
+}
